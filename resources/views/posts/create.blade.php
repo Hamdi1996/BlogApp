035944a83094fe4906ef25a033b4af583c9f6668
Hamdi Ellithy <hamedy_mhmoud@yahoo.com>
2020-03-14 23:58:49 +0200

    Final step deploy

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,8 +1,9 @@
 @extends ('layouts.app')
         
-      
+  
 @section('content')
- 
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script>tinymce.init({selector:'textarea'});</script> 
 
 {!! Form::open(['action' => 'PostsController@store','method'=>'POST','enctype'=>'multipart/form-data']) !!}
 <div class="container">
